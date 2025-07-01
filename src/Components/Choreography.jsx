01e5db860de762d6a9c11cf19bd76e6d16e3e35e
BECCABOOTY <vintagefreak001@gmail.com>
2025-07-01 09:14:19 +0100

    setup meaning

---
+++ src/Components/Choreography.jsx
@@ -0,0 +1,5 @@
+import "animate.css";
+
+export default function Choreography() {
+  return <></>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Meaning from "@/Components/Meaning";
+
 export default function Home() {
   return (
     <>
       <h1>Home Page</h1>
+      <Meaning />
     </>
   );
 }
