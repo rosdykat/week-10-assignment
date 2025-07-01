2df67d2f6d119255bc9ecc136f6f307596f50713
Ross Fletcher <rossfletcher108@gmail.com>
2025-07-01 10:54:22 +0100

    working physics on click

--- src/Components/Physics.jsx
+++ src/Components/Physics.jsx
@@ -3,29 +3,40 @@
 import "animate.css";
 import style from "@/Components/physics.module.css";
 
-function PhysicsClick() {
-  const element = document.querySelector(`.${style.bigBox}`);
+function SlowClick() {
+  const element = document.querySelector(".bigBox");
   element.classList.remove("animate__bounce", "animate__slower");
+  void element.offsetWidth;
   element.classList.add("animate__bounce", "animate__slower");
+}
+
+function MediumClick() {
+  const element = document.querySelector(".mediumBox");
+  element.classList.remove("animate__bounce", "animate__slow");
+  void element.offsetWidth;
+  element.classList.add("animate__bounce", "animate__slow");
+}
+
+function FastClick() {
+  const element = document.querySelector(".smallBox");
+  element.classList.remove("animate__bounce", "animate__fast");
+  void element.offsetWidth;
+  element.classList.add("animate__bounce", "animate__fast");
 }
 
 export default function PhysicsComponent() {
   return (
     <div className={style.boxContainer}>
-      <div
-        className={`${style.bigBox} animate__animated animate__bounce animate__slower`}
-      >
-        <button onClick={PhysicsClick}>Click me!</button>
+      <div className={`${style.bigBox} animate__animated bigBox`}>
+        <button onClick={SlowClick}>Click me!</button>
+      </div>
+      <div className={`${style.mediumBox} animate__animated mediumBox`}>
+        <button onClick={MediumClick}>Click me!</button>
       </div>
       <div
-        className={`${style.mediumBox} animate__animated animate__bounce animate__slow`}
+        className={`${style.smallBox} animate__animated animate__bounce animate__fast smallBox`}
       >
-        <button>Click me!</button>
-      </div>
-      <div
-        className={`${style.smallBox} animate__animated animate__bounce animate__fast`}
-      >
-        <button>Click me!</button>
+        <button onClick={FastClick}>Click me!</button>
       </div>
     </div>
   );
